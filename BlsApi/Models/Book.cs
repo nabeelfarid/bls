d7282a4e1a2c91df4829fa253aafdae71d6da1ac
nabeelfarid <nabeelfarid@gmail.com>
2025-10-27 01:28:18 +0000

    Fixed JSON deserialization to support lowercase property names

--- BlsApi/Models/Book.cs
+++ BlsApi/Models/Book.cs
@@ -1,13 +1,23 @@
 using System;
+using System.Text.Json.Serialization;
 
 namespace BlsApi.Models
 {
     public class Book
     {
-        public required string Id { get; set; }
+        [JsonPropertyName("id")]
+        public string? Id { get; set; }
+        
+        [JsonPropertyName("title")]
         public required string Title { get; set; }
+        
+        [JsonPropertyName("author")]
         public required string Author { get; set; }
+        
+        [JsonPropertyName("isbn")]
         public required string ISBN { get; set; }
+        
+        [JsonPropertyName("isCheckedOut")]
         public bool IsCheckedOut { get; set; } = false;
     }
 }
